3a276546ea8547c6f76bf82d628f48dcb32c3ded
MasSagita <40248554+MasSagita@users.noreply.github.com>
2023-05-29 10:12:17 +0700

    Add files via upload

---
+++ Sealing_Cup/Sealing_Cup.ino
@@ -0,0 +1,256 @@
+#include <EEPROM.h>
+
+#include "input_output.h"
+
+int pw_speed; // variable speed power window
+
+int speed_in, speed_out;
+
+void (* resetFunc) (void) = 0;
+
+int in_delay, set_in_delay;
+int out_delay, set_out_delay;
+
+void setup() {
+  // put your setup code here, to run once:
+  pw_speed = EEPROM.read(0);
+  set_in_delay = EEPROM.read(1);
+  set_out_delay = EEPROM.read(2);
+  speed_in = EEPROM.read(3);
+  speed_out = EEPROM.read(4);
+
+  in_delay = set_in_delay * 100;
+  out_delay = set_out_delay * 100;
+
+  Serial.begin(115200);
+  Serial.println("CUP SEALER");
+  setup_input_output();
+  homing_front();
+}
+
+int menu;
+int cursor_menu;
+
+int depan = 0, belakang = 1, atas = 2;
+
+int counter_tes;
+
+int test_menu = 0;
+
+void loop() {
+  digitalWrite(led_pin, !digitalRead(led_pin));
+  refresh_screen(5);
+  short_long_btn(2);
+  test_menu = 0;
+
+  lcd.setCursor(0, 0), lcd.print(F("    Stand By"));
+  lcd.setCursor(5, 1), lcd.print(total_cup), lcd.print(F("cup"));
+  if (is_long_pressing) setting_menu();
+
+  counter_tes++;
+  if (counter_tes > 10) counter_tes = 0;
+  lcd.setCursor(14, 1), lcd.print(counter_tes);
+  counter_cup();
+  go_sealing();
+
+}
+
+// move inside
+void go_sealing() {
+  if (ls(atas) && ir_state() || test_menu == 1 && !button(3)) {
+    led_blink(100, 100);
+    lcd.clear();
+    lcd.setCursor(5, 0), lcd.print(F("Waiting"));
+    delay(in_delay);
+    beep_once(50);
+    // 90 adalah kecepatan yang ideal
+    run_motor(speed_in, 1);
+    lcd.clear();
+    while (1) {
+      led_blink(100, 100);
+      lcd.setCursor(3, 0), lcd.print("Go Inside");
+      if (ls(belakang)) run_motor(0, 1), beep_once(50), delay(500);
+      if (!ls(atas)) {
+        lcd.clear();
+        break;
+      }
+    }
+    while (1) {
+      led_blink(100, 100);
+      //      Serial.print(ls(belakang));
+      //      Serial.print(" ");
+      //      Serial.println(ls(atas));
+      lcd.setCursor(4, 0), lcd.print(F("Sealing"));
+      if (ls(atas) && ls(belakang)) {
+        //        Serial.print(ls(belakang));
+        //        Serial.print(" ");
+        //        Serial.println(ls(atas));
+        led_blink(100, 100);
+        delay(out_delay);
+        beep_once(100);
+        lcd.clear();
+        break;
+      }
+    }
+    go_front();
+  }
+}
+
+// move outside
+void go_front() {
+  while (1) {
+    led_blink(100, 250);
+    lcd.setCursor(3, 0), lcd.print(F("Go Outside"));
+    if (ls(atas) && ls(belakang)) {
+      run_motor(speed_out, 1);
+    }
+    if (ls(depan)) {
+      lcd.clear();
+      lcd.setCursor(0, 0), lcd.print(F("  Remove Gelas"));
+      run_motor(0, 1), beep_once(250);
+      if (!ir_state() || (menu >= 0 && menu <= 1 && cursor_menu == 1)) {
+        break;
+      }
+    }
+  }
+}
+
+// mengembalikan hopper ke posisi depan
+void homing_front() {
+  lcd.setCursor(0, 0), lcd.print(F("  Homing Front"));
+  while (1) {
+    refresh_screen(5);
+    if (!ls(depan)) run_motor(speed_out, 1);
+    if (ls(depan)) {
+      lcd.setCursor(0, 0), lcd.print(F("  Remove Gelas"));
+      run_motor(0, 1);
+      delay(250), beep_once(100);
+      if (!ir_state()) {
+        lcd.setCursor(0, 0), lcd.print(F("  Done Homing"));
+        delay(250), beep_once(250);
+        break;
+      }
+    }
+  }
+}
+
+// menu untuk setting parameter mesin
+void setting_menu() {
+  menu = 0, cursor_menu = 0;
+  beep_once(50);
+  lcd.clear();
+  lcd.setCursor(0, 0), lcd.print("  Setting Menu");
+  delay(1000);
+  while (1) {
+    led_blink(100, 1000);
+    short_long_btn(2); //button 2 = button enter
+    refresh_screen(5);
+
+    if (!button(2) && cursor_menu == 0) menu += 1, beep_once(25), delay(100);
+
+    if (menu > 6) menu = 0;
+    if (menu < 0) menu = 6;
+
+    lcd.setCursor(14, 1), lcd.print(menu);
+
+    if (menu >= 5 && menu <= 6) {
+      if (!button(0) || !button(1)) cursor_menu += 1, beep_once(25), delay(100);
+      if (cursor_menu > 1) cursor_menu = 0;
+      if (cursor_menu < 0) cursor_menu = 1;
+      if (cursor_menu == 0) lcd.setCursor(0, 0), lcd.write(0);
+      if (cursor_menu == 1) lcd.setCursor(0, 1), lcd.write(0);
+    }
+
+    if (menu != 5 && menu != 6) lcd.setCursor(0, 1), lcd.write(0);
+
+    // setting kecepatan motor ke belakang
+    if (menu == 0) {
+      test_menu = 1;
+      lcd.setCursor(1, 0), lcd.print("Speed Go In");
+      if (!button(0)) speed_in--, beep_once(25);
+      if (!button(1)) speed_in++, beep_once(25);
+      if (speed_in < 0) speed_in = 0;
+      if (speed_in > 255) speed_in = 255;
+      lcd.setCursor(1, 1), lcd.print(speed_in), lcd.print("pwm");
+      if (!button(3)) go_sealing();
+    }
+    // setting kecepatan motor ke depan
+    if (menu == 1) {
+      test_menu = 1;
+      lcd.setCursor(1, 0), lcd.print("Speed Go Out");
+      if (!button(0)) speed_out--, beep_once(25);
+      if (!button(1)) speed_out++, beep_once(25);
+      if (speed_out < 0) speed_out = 0;
+      if (speed_out > 255) speed_out = 255;
+      lcd.setCursor(1, 1), lcd.print(speed_out), lcd.print("pwm");
+      if (!button(3)) go_sealing();
+    }
+    // setting waktu tunggu sebelum cup menuju ke belakang
+    if (menu == 2) {
+      lcd.setCursor(1, 0), lcd.print("Delay IN");
+      if (!button(0)) set_in_delay -= 1, beep_once(25);
+      if (!button(1)) set_in_delay += 1, beep_once(25);
+      if (set_in_delay < 0) set_in_delay = 0;
+      if (set_in_delay > 100) set_in_delay = 100;
+      in_delay = set_in_delay * 100;
+      lcd.setCursor(1, 1), lcd.print(in_delay), lcd.print("ms");
+    }
+    // setting waktu tunggu sebelum cup menuju ke depan
+    if (menu == 3) {
+      lcd.setCursor(1, 0), lcd.print("Delay OUT");
+      if (!button(0)) set_out_delay -= 1, beep_once(25);
+      if (!button(1)) set_out_delay += 1, beep_once(25);
+      if (set_out_delay < 0) set_out_delay = 0;
+      if (set_out_delay > 70) set_out_delay = 70;
+      out_delay = set_out_delay * 100;
+      lcd.setCursor(1, 1), lcd.print(out_delay), lcd.print("ms");
+    }
+    // cek sensor
+    if (menu == 4) {
+      lcd.setCursor(1, 0), lcd.print(F("IR "));
+      lcd.setCursor(4, 0), lcd.print(F("LD "));
+      lcd.setCursor(7, 0), lcd.print(F("LB "));
+      lcd.setCursor(10, 0), lcd.print(F("LA "));
+      lcd.setCursor(1, 1), lcd.print(ir_state());
+      lcd.setCursor(4, 1), lcd.print(ls(depan));
+      lcd.setCursor(7, 1), lcd.print(ls(belakang));
+      lcd.setCursor(10, 1), lcd.print(ls(atas));
+    }
+    // kembali ke standby
+    if (menu == 5) {
+      lcd.setCursor(1, 0), lcd.print("Selesai?");
+      lcd.setCursor(1, 1), lcd.print("Mulai");
+      if (cursor_menu == 1 && button(2) == 0) {
+        while (1) {
+          lcd.clear();
+          lcd.setCursor(0, 0), lcd.print("     MULAI");
+          beep_once(200), beep_once(50);
+          cursor_menu = 0, menu = 0;
+          break;
+        }
+        break;
+      }
+    }
+    // save setting ke eeprom
+    if (menu == 6) {
+      lcd.setCursor(1, 0), lcd.print("Selesai?");
+      lcd.setCursor(1, 1), lcd.print("Save & Reset");
+      if (cursor_menu == 1 && button(2) == 0) {
+        beep_once(250);
+        delay(1000), lcd.clear();
+        lcd.setCursor(0, 0), lcd.print("  Save & Reset");
+        EEPROM.write(0, pw_speed);
+        EEPROM.write(1, set_in_delay);
+        EEPROM.write(2, set_out_delay);
+        EEPROM.write(3, speed_in);
+        EEPROM.write(4, speed_out);
+        for (int i = 0; i < 16; i++) {
+          lcd.setCursor(i, 1), lcd.print(F("."));
+          delay(100);
+        }
+        resetFunc();
+      }
+    }
+  }
+}
+
---
+++ Sealing_Cup/input_output.h
@@ -0,0 +1,229 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+//char panah
+byte customChar[] = {
+  B00000,
+  B00100,
+  B00010,
+  B11111,
+  B00010,
+  B00100,
+  B00000,
+  B00000
+};
+
+// pin adc button
+int btn_analog_pin = A0;
+
+// pin sensor Limit Switch
+const int ls_pin[3] = {A1, A2, A3};
+
+// pin sensor infrared
+int ir_pin = A7;
+
+// pin led dan buzzer
+const int led_pin = 13;
+const int buz_pin = 12;
+
+// motor power window pin
+const int motor_pwm_pin = 6;
+const int motor_left_pin = 7;
+const int motor_right_pin = 8;
+
+// setup io
+void setup_input_output() {
+  lcd.init();
+  pinMode(btn_analog_pin, INPUT_PULLUP);
+  
+  pinMode(ls_pin[0], INPUT);
+  pinMode(ls_pin[1], INPUT_PULLUP);
+  pinMode(ls_pin[2], INPUT_PULLUP);
+  
+  pinMode(ir_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  pinMode(buz_pin, OUTPUT);
+  
+  pinMode(motor_pwm_pin, OUTPUT);
+  pinMode(motor_left_pin, OUTPUT);
+  pinMode(motor_right_pin, OUTPUT);
+  
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(buz_pin, !digitalRead(buz_pin));
+    digitalWrite(led_pin, !digitalRead(led_pin));
+    delay(35);
+  }
+  lcd.createChar(0, customChar);
+  lcd.backlight();
+  lcd.setCursor(0, 0), lcd.print("   CUP SEALER");
+  delay(2000);
+  lcd.clear();
+}
+
+//fungsi run motor power window
+void run_motor(int spd, int brk) {
+  if (spd == 0 && brk == 1) {
+    analogWrite(motor_pwm_pin, 255);
+    digitalWrite(motor_left_pin, 0);
+    digitalWrite(motor_right_pin, 0);
+  }
+  if (spd == 0 && brk == 0) {
+    analogWrite(motor_pwm_pin, 0);
+    digitalWrite(motor_left_pin, 0);
+    digitalWrite(motor_right_pin, 0);
+  }
+  if (spd > 0) {
+    analogWrite(motor_pwm_pin, spd);
+    digitalWrite(motor_left_pin, 1);
+    digitalWrite(motor_right_pin, 0);
+  }
+  if (spd < 0) {
+    analogWrite(motor_pwm_pin, -spd);
+    digitalWrite(motor_left_pin, 0);
+    digitalWrite(motor_right_pin, 1);
+  }
+}
+
+// fungsi blinking led
+unsigned long prev_millis_led;
+int led_state = LOW;
+void led_blink(int on_time, int off_time) {
+  unsigned long current_millis_led = millis();
+  if ((led_state == 1) && (current_millis_led - prev_millis_led >= on_time)) {
+    led_state = 0;
+    prev_millis_led = current_millis_led;
+  }
+  else if ((led_state == 0) && (current_millis_led - prev_millis_led >= off_time)) {
+    led_state = 1;
+    prev_millis_led = current_millis_led;
+  }
+  digitalWrite(led_pin, led_state);
+  //if(ch == 1) digitalWrite(buzzer_pin, led_state);
+}
+
+// fungsi beep buzzer 1 kali
+void beep_once(int interval) {
+  digitalWrite(buz_pin, HIGH);
+  delay(interval);
+  digitalWrite(buz_pin, LOW);
+  delay(interval);
+}
+
+// fungsi baca sensor ir
+bool ir_state() {
+  boolean x;
+  int ir_val = analogRead(ir_pin);
+  if (ir_val < 50) x = 1;
+  else x = 0;
+  return x;
+}
+
+// infrared untuk counter cup
+int last_state_ir = LOW;
+int ir_counter = 0;
+int total_cup = 0;
+void counter_cup() {
+  if (!last_state_ir && ir_state()) {
+    ir_counter += 1;
+    total_cup += 1;
+  }
+  last_state_ir = ir_state();
+}
+
+// fungsi baca sensor limit switch
+int adc_ls[3];
+boolean ls(int pin) {
+  boolean x;
+  for (int i = 0; i < 3; i++) {
+    adc_ls[i] = analogRead(ls_pin[i]);
+  }
+  if (adc_ls[pin] < 100) x = 1;
+  else x = 0;  
+  return x;
+}
+
+//Push Button ADC
+// Settingan sealer cup
+int kalibrasi_btn;
+bool mode_button_usb = true; // true while usb on
+boolean button(int i) {
+  if (mode_button_usb) {
+    if (i == 3) kalibrasi_btn = -70;
+    else kalibrasi_btn = -18;
+  }
+  if (!mode_button_usb) kalibrasi_btn = 5;
+  boolean y = 1;
+  int adcButton = analogRead(btn_analog_pin);
+  if (i == 0) {
+    if (adcButton >= 95 + kalibrasi_btn && adcButton <= 105 + kalibrasi_btn) y = 0;
+    else y = 1;
+  }
+  if (i == 1) {
+    if (adcButton >= 110 + kalibrasi_btn && adcButton <= 125 + kalibrasi_btn) y = 0;
+    else y = 1;
+  }
+  if (i == 2) {
+    if (adcButton >= 135 + kalibrasi_btn && adcButton <= 145 + kalibrasi_btn) y = 0;
+    else y = 1;
+  }
+  if (i == 3) {
+    if (adcButton >= 350 + kalibrasi_btn && adcButton <= 370 + kalibrasi_btn) y = 0;
+    else y = 1;
+  }
+  return y;
+}
+
+
+// fungsi dan variable long press button
+const int short_press_time = 500;
+const int long_press_time = 800;
+
+int last_state_rtry = LOW;  // previous state
+int current_state_rtry;     // current reading
+unsigned long pressed_time = 0;
+unsigned long realesed_time = 0;
+bool is_pressing = false;
+bool is_long_pressing = false;
+
+bool long_press_btn = false;
+bool short_press_btn = false;
+
+void short_long_btn(int pin) {
+  current_state_rtry = button(pin);
+  if (last_state_rtry == HIGH && current_state_rtry == LOW) {
+    pressed_time = millis();
+    is_pressing = true;
+    is_long_pressing = false;
+  } else if (last_state_rtry == LOW && current_state_rtry == HIGH) {
+    is_pressing = false;
+    realesed_time = millis();
+    long press_duration = realesed_time - pressed_time;
+    if (press_duration < short_press_time) {
+      long_press_btn = false;
+      short_press_btn = true;
+      Serial.print("short press ");
+      Serial.println(pressed_time);
+    }
+  }
+  if (is_pressing == true && is_long_pressing == false) {
+    long press_duration = millis() - pressed_time;
+    if (press_duration > long_press_time) {
+      long_press_btn = true;
+      short_press_btn = false;
+      Serial.print("long press");
+      Serial.println(pressed_time);
+      is_long_pressing = true;
+    }
+  }
+  last_state_rtry = current_state_rtry;
+}
+
+// fungsi refresh lcd 
+int refresh;
+void refresh_screen(int intervalRefresh) {
+  if (++refresh > intervalRefresh) {
+    lcd.clear();
+    refresh = 0;
+  }
+}
+
